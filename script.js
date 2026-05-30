3306fcb71d5a88b81b1d698f3e42ec66c44b6f45
Bhavesh Kuamawat <bhavesh461@outlook.com>
2026-05-30 11:43:13 +0000

    Make default hovering color as random and add gap between button and grid

--- script.js
+++ script.js
@@ -7,32 +7,37 @@ function grid() {
     div.innerHTML = "";
 
     const size = parseInt(prompt("Enter the size of grid"));
-    
-    if(size>0&&size<=100){
-    const total = size*size;
 
-    for (let i = 1; i <= total; i++) {
-        const box = document.createElement("div");
-        box.classList.add("box");
-        div.appendChild(box);
+    if (size > 0 && size <= 100) {
+        const total = size * size;
 
-        box.addEventListener("mouseenter", () => {
-            box.style.backgroundColor = "yellow";
-        });
+        for (let i = 1; i <= total; i++) {
+            const box = document.createElement("div");
+            box.classList.add("box");
+            div.appendChild(box);
+
+            box.addEventListener("mouseenter", () => {
+                const r = Math.floor(Math.random() * 256);
+                const g = Math.floor(Math.random() * 256);
+                const b = Math.floor(Math.random() * 256);
+
+                box.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+            });
+        }
+
+        const width = 640 / size;
+        const height = 640 / size;
+        const target = document.querySelectorAll(".box");
+
+        for (const box of target) {
+            box.style.width = `${width}px`;
+            box.style.height = `${height}px`;
+        }
     }
 
-    const width = 640 / size;
-    const height = 640 / size;
-    const target = document.querySelectorAll(".box");
-
-    for(const box of target){
-    box.style.width = `${width}px`;
-    box.style.height = `${height}px`;
-    }}
-
-    else{
-        alert("Please enter a valid size, which is less than 100!")
+    else {
+        alert("Please enter a valid size, which is less than 100!");
     }
 }
 
-document.querySelector("button").addEventListener("click",grid);
+document.querySelector("button").addEventListener("click", grid);
