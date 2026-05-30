899dec900e299c3cb2db5cec32a01f0475e917d0
Bhavesh Kuamawat <bhavesh461@outlook.com>
2026-05-30 11:12:54 +0000

    Create a grid with DOM and add colors when hovered around it
    I have created a grid with help of user input and added hovering effect with use of events, currently it only support one color.

---
+++ script.js
@@ -0,0 +1,38 @@
+const div = document.createElement("div");
+div.classList.add("container");
+document.body.appendChild(div);
+
+function grid() {
+
+    div.innerHTML = "";
+
+    const size = parseInt(prompt("Enter the size of grid"));
+    
+    if(size>0&&size<=100){
+    const total = size*size;
+
+    for (let i = 1; i <= total; i++) {
+        const box = document.createElement("div");
+        box.classList.add("box");
+        div.appendChild(box);
+
+        box.addEventListener("mouseenter", () => {
+            box.style.backgroundColor = "yellow";
+        });
+    }
+
+    const width = 640 / size;
+    const height = 640 / size;
+    const target = document.querySelectorAll(".box");
+
+    for(const box of target){
+    box.style.width = `${width}px`;
+    box.style.height = `${height}px`;
+    }}
+
+    else{
+        alert("Please enter a valid size, which is less than 100!")
+    }
+}
+
+document.querySelector("button").addEventListener("click",grid);
